dcc139e1ec5206f24be70ca15fdb3dc1ec3f70dd
waynejsk <50647984+waynejsk@users.noreply.github.com>
2019-06-09 17:13:13 +0900

    Boyer-Moore法

--- BMSearch.scala
+++ BMSearch.scala
@@ -1,5 +1,45 @@
 object BMSearch extends App {
-  val text = "カワカドカドカドドワンゴカドカドンゴドワドワンゴドワカワカドンゴドワ".toSeq
+  val text "カワカドカドカドドワンゴカドカドンゴドワドワンゴドワカワカドンゴドワ".toSeq
   val pattern = "ドワンゴ".toSeq
+  val skipTable = pattern.map(s => (pattern.reverse.indexOf(s))).toMap
+  println("skipTable: " + skipTable) // Map(ド -> 3, ワ -> 2, ン-> 1,　ゴ -> 0)
+  val matchIndexes = search(text, pattern)
 
+  def search(text: Seq[Char], pattern: Seq[Char]): Seq[Int] = {
+    var matchIndexes = Seq[Int]()
+    var i = 0
+
+    while (i < text.length - 1) {
+      val partText = text.slice(i, i + pattern.length)
+      println("partText: " + partText)
+      val patternLastIndex = pattern.length - 1 // 「パターン」の最後の文字の添え字
+
+      var siMatch = true
+      var j = patternLastIndexvar matchChar = '_' //マッチさせた際の「文書」の文字
+      var matchPosition = 0 //マッチさせた際の位置（スキップテーブルから取得した値を差し引く値）
+
+      while (j >= 0 && isMatch) {
+        if (j > partText.length - 1) { //切り出しテキストが短い際にはfalseに
+        isMatch = false
+        } else {
+          matchChar != pattern(j)) {
+            isMatch = false
+            matchPosition = (patternLastIndex - j)  // 入り版後ろで不一致なら0　後ろから2番目の時に不一致なら1が代入
+          }
+        }
+        j = j - 1
+      }
+
+      if (isMatch) matchIndexes = matchIndexes :+ i
+
+      var skipCount = skipTable.getOrElse(matchChar, pattern.length) - matchPosition
+      if (skipCount <= 0) skipCount = 1
+      println("skipCount: " + skipCount)
+      i = i + skipCount      
+    }
+
+    matchIndexes
+  }
+
+  println(s"出現場所: ${matchIndexes}")
 }
