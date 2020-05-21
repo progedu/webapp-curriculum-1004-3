3bd6cdf567c4024c9f85f8caeafa8562e854faa5
hir-810 <hir81054@gmail.com>
2020-05-21 12:28:03 +0900

    BMSearch実装

--- BMSearch.scala
+++ BMSearch.scala
@@ -1,5 +1,36 @@
 object BMSearch extends App {
-  val text = "カワカドカドカドドワンゴカドカドンゴドワドワンゴドワカワカドンゴドワ".toSeq
-  val pattern = "ドワンゴ".toSeq
+    val text = "カワカドカドカドドワンゴカドカドンゴドワドワンゴドワカワカドンゴドワ".toSeq
+    val pattern = "ドワンゴ".toSeq
+
+    def createIndex(pattern: Seq[Char]) : Map[Char,Int] = {
+        var res = Map[Char,Int]()
+        for (i <- 0 until pattern.length) {
+            val x = pattern.length-i-1
+            res += (pattern(i) -> x)
+        }
+        res
+    }
+
+    val bmIndex = createIndex(pattern)
+    println(bmIndex)
+
+    var matchIndexes = Seq[Int]()
+
+    var i = 0
+    while(i+pattern.length-1 < text.length) {
+        var ok = true
+        var j = pattern.length -1
+        while(ok && j >= 0) {
+            println(i,j)
+            if (text(i+j) != pattern(j)) ok = false
+            else j -= 1
+        }
+        if (ok) {
+            matchIndexes = matchIndexes :+ i
+            i += pattern.length
+        } else i += bmIndex.getOrElse(text(i+j),pattern.length)
+    }
+
+    println(matchIndexes)
 
 }
